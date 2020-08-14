2d04438d9862ef10b6483730be0810220ce59621
Hunter Jennings <jenningsdhunter@gmail.com>
2020-08-14 09:16:20 -0400

    Adding proper config files for jest to run with TS

--- frontend/src/App.spec.tsx
+++ frontend/src/App.spec.tsx
@@ -1,7 +1,3 @@
-/* import React from 'react'; */
-/* import { render } from '@testing-library/react'; */
-/* import { Navigation } from './components/Navigation/Navigation'; */
-
 test('runs the first test', () => {
   expect(true).toBe(true);
 });
